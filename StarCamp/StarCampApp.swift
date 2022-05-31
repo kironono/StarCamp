84ffdb756c99fbc5e1041b8301a24c496214491a
Junichi Kakisako <kilo.sw@gmail.com>
2022-05-31 23:16:28 +0900

    Initial Commit

---
+++ StarCamp/StarCampApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarCampApp.swift
+//  StarCamp
+//
+//  Created by Junichi Kakisako on 2022/05/31.
+//
+
+import SwiftUI
+
+@main
+struct StarCampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
